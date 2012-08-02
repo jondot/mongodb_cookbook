9332248f107e1eab9e9a7c8c89967a3b0e092639
Dotan J. Nahum <jondotan@gmail.com>
2012-08-02 12:17:55 +0300

    Update recipes/stand-alone.rb

--- recipes/stand-alone.rb
+++ recipes/stand-alone.rb
@@ -1,10 +1,10 @@
 # Installs a stand-alone MongoDB instance
 
-include_recipe "mongodb::_dependencies"
-include_recipe "mongodb::_add_upstart_repository"
-include_recipe "mongodb::_package"
-include_recipe "mongodb::_group"
-include_recipe "mongodb::_user"
-include_recipe "mongodb::_directories"
-include_recipe "mongodb::_service"
-include_recipe "mongodb::_config"
+include_recipe "nistude_mongodb::_dependencies"
+include_recipe "nistude_mongodb::_add_upstart_repository"
+include_recipe "nistude_mongodb::_package"
+include_recipe "nistude_mongodb::_group"
+include_recipe "nistude_mongodb::_user"
+include_recipe "nistude_mongodb::_directories"
+include_recipe "nistude_mongodb::_service"
+include_recipe "nistude_mongodb::_config"
